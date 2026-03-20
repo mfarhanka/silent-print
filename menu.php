6d8f6ca4bbde631510a4249354674bdc91ed2dae
Farhan Kamarzaman <farhan@dzvisual.my>
2026-03-20 16:23:27 +0800

    Fix product link in menu and add products page with detailed product listings

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <div class="collapse navbar-collapse order-lg-0" id="navbarNav">
     <ul class="navbar-nav small">
-        <li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+        <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Latest Features</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Packages</a></li>
         <li class="nav-item"><a class="nav-link" href="#">Process</a></li>
---
+++ products.php
@@ -0,0 +1,84 @@
+<?php include 'header.php'; ?>
+
+<!-- Products Page Content -->
+<section class="py-5">
+    <div class="container">
+        <h2 class="fw-bold mb-4">Our Products</h2>
+        <div class="row g-4">
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/calendars.png" class="card-img-top" alt="Calendars">
+                    <div class="card-body">
+                        <h5 class="card-title">Calendars</h5>
+                        <p class="card-text">High-quality custom calendars for your business or personal use.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/notebooks.png" class="card-img-top" alt="Notebooks">
+                    <div class="card-body">
+                        <h5 class="card-title">Notebooks</h5>
+                        <p class="card-text">Custom notebooks for notes, journaling, and branding.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/paper-bags.png" class="card-img-top" alt="Paper Bags">
+                    <div class="card-body">
+                        <h5 class="card-title">Paper Bags</h5>
+                        <p class="card-text">Eco-friendly paper bags for retail and events.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/business-cards.png" class="card-img-top" alt="Business Cards">
+                    <div class="card-body">
+                        <h5 class="card-title">Business Cards</h5>
+                        <p class="card-text">Professional business cards to make a lasting impression.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/envelopes.png" class="card-img-top" alt="Envelopes">
+                    <div class="card-body">
+                        <h5 class="card-title">Envelopes</h5>
+                        <p class="card-text">Custom printed envelopes for your business needs.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/folder-sets.png" class="card-img-top" alt="Folder Sets">
+                    <div class="card-body">
+                        <h5 class="card-title">Folder Sets</h5>
+                        <p class="card-text">Organize documents with personalized folder sets.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/food-packing.png" class="card-img-top" alt="Food Packaging">
+                    <div class="card-body">
+                        <h5 class="card-title">Food Packaging</h5>
+                        <p class="card-text">Safe and attractive packaging for food products.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100">
+                    <img src="img/products/marketing-materials.png" class="card-img-top" alt="Marketing Materials">
+                    <div class="card-body">
+                        <h5 class="card-title">Marketing Materials</h5>
+                        <p class="card-text">Flyers, brochures, and more to promote your business.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
